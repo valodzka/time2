b654e4677ebac1b7ed23fa6bcd8a2a9d8f1df943
Pavel Valodzka <pavel@valodzka.name>
2009-03-19 13:40:44 +0200

    more tests

---
+++ test/test_posix_tz.rb
@@ -0,0 +1,11 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class TestPosixTZ < Test::Unit::TestCase
+  def test_plus
+    with_tz('UTC+100') {
+      t = Time.now
+      #TODO: should this work? assert_equal "UTC+100", t.zone
+      assert_equal -100*3600, t.gmtoff
+    }
+  end
+end
